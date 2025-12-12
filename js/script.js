e6eaa8656c2fd1ddd2ca08c60fd7766ae611937c
JBragaV <jocimarcaiadobraga@gmail.com>
2025-12-12 13:52:44 -0300

    JS finalizado?

--- js/script.js
+++ js/script.js
@@ -1,38 +1,51 @@
+// Funções
 const pegarElemento = (id) => document.getElementById(id)
+const apagarTabelaUsuários = () => {
+    const tabela = document.getElementById('tabela-funcionarios')
+    if (tabela) {
+        tabela.remove()
+    }
+    
+}
 const criarTabelaUsuarios = (dados) => {
     const sessaoTabela = pegarElemento('section-table-list')
-    
-    // Criação dinâmica da tabela
-    const tabela = document.createElement('table')
-    
-    // Criação do cabeçalho da tabela
-    const tabelaHead = document.createElement('thead')
-    const headLinhaNome = document.createElement('th')
-    const headLinhaEmail = document.createElement('th')
-    const headLinhaAcao = document.createElement('th')
-    
-    headLinhaNome.innerText = "Nome"
-    headLinhaEmail.innerText = "E-mail"
-    headLinhaAcao.innerText = "Ação"
+    if (dados.length == 0){
+        // TODO inserir informação que não existe usuários
+        
+    }else{
 
-    // Criação do corpo da tabela
-    const tabelaBody = document.createElement('tbody')
-
-    dados.forEach((dado) => {
-        let elemento = dadosFuncionarioTabela(dado)
-        tabelaBody.appendChild(elemento)
-    })
-    // Montanto a tabela dinâmicamente
-    tabelaHead.appendChild(headLinhaNome)
-    tabelaHead.appendChild(headLinhaEmail)
-    tabelaHead.appendChild(headLinhaAcao)
-    tabela.appendChild(tabelaHead)
-    tabela.appendChild(tabelaBody)
-    sessaoTabela.appendChild(tabela)
+        // Criação dinâmica da tabela
+        const tabela = document.createElement('table')
+        tabela.id = 'tabela-funcionarios'
+        
+        // Criação do cabeçalho da tabela
+        const tabelaHead = document.createElement('thead')
+        const headLinhaNome = document.createElement('th')
+        const headLinhaEmail = document.createElement('th')
+        const headLinhaAcao = document.createElement('th')
+        
+        headLinhaNome.innerText = "Nome"
+        headLinhaEmail.innerText = "E-mail"
+        headLinhaAcao.innerText = "Ação"
+    
+        // Criação do corpo da tabela
+        const tabelaBody = document.createElement('tbody')
+    
+        dados.forEach((dado) => {
+            let elemento = dadosFuncionarioTabela(dado)
+            tabelaBody.appendChild(elemento)
+        })
+        // Montanto a tabela dinâmicamente
+        tabelaHead.appendChild(headLinhaNome)
+        tabelaHead.appendChild(headLinhaEmail)
+        tabelaHead.appendChild(headLinhaAcao)
+        tabela.appendChild(tabelaHead)
+        tabela.appendChild(tabelaBody)
+        sessaoTabela.appendChild(tabela)
+    }
 }
 
 const dadosFuncionarioTabela = (dado) => {
-    console.log(dado)
     const tabelaBodyLinha = document.createElement('tr')
     const nome = document.createElement('td')
     const email = document.createElement('td')
@@ -46,25 +59,17 @@ const dadosFuncionarioTabela = (dado) => {
     return tabelaBodyLinha
 }
 
-
-const baseUrl = 'https://crudcrud.com/api/aa154cd1f28c4a74aebbf5afb76d5831/funcionario'
+// Variáveis
+const baseUrl = 'https://crudcrud.com/api/0469d2885c6247f6b60a2f885d121213/funcionario'
 const formulario = pegarElemento('add-form-woker')
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetch(baseUrl)
-    .then(response => {
-        if (!response.ok) throw new Error("Erro ao buscar")
-        return response.json()
-    }).then(dados => {
-        console.table(dados)
-        criarTabelaUsuarios(dados)
-    }).catch(error => console.error(`MEU ERRO FOI ${error.status}`))
+    listarFuncionarios()
 })
 
 
 formulario.addEventListener('submit', (e) => {
-    e.preventDefault()
     const nome = pegarElemento('nome-worker').value
     const email = pegarElemento('email-worker').value
     console.log(nome, email)
@@ -77,25 +82,40 @@ function cadastrarCliente(nome, email) {
         "Content-Type": "application/json"
     }
     
-    fetch(baseUrl, {
+    return fetch(baseUrl, {
         method: 'POST',
         headers: header,
         body: JSON.stringify({"nome": nome, "email": email})
     }).then(response => {
         if (!response.ok) throw new Error(`Erro na requisição. Status Code ${response.status}`)
-        return response.json()
-    }).then(dados => {
-        console.log(dados)
-    }).catch(erro => console.error(`O erro foi esse: ${erro}`))
+        return response
+    }).catch(erro => {
+        console.error(`O erro foi esse: ${erro}`)
+    })
 }
 
 function apagarFuncionario(id) {
     console.log(`${baseUrl}/${id}`)
     fetch(`${baseUrl}/${id}`, {method: 'DELETE'})
     .then(response => {
+        console.log(response)
         if(!response.ok) throw new Error(`Erro ao apagar o funcionário. Status ${response.status}`)
-        return response.json()
-    }).then(dado => console.log(dado))
-    .catch(erro => console.error("Houve um erro ao deletar\n"+erro))
+        apagarTabelaUsuários()
+        listarFuncionarios()
+    }).catch(erro => console.error("Houve um erro ao deletar\n"+erro))
 
+}
+
+function listarFuncionarios() {
+    fetch(baseUrl)
+    .then(response => {
+        if (!response.ok) throw new Error("Erro ao buscar")
+        return response.json()
+    }).then(dados => {
+        console.table(dados)
+        criarTabelaUsuarios(dados)
+    }).catch(error => {
+        console.error(`MEU ERRO FOI ${error}`)
+        // TODO Informar que houve um erro na API.
+    })
 }
